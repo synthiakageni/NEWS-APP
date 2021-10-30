527e93a7b380f5a808a7856087ff9c595f6198c6
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-30 20:20:30 +0300

    create funtion index()

--- app/views.py
+++ app/views.py
@@ -25,9 +25,15 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    # Getting popular movie
-    popular_news = get_news('popular')
-    print(popular_news)
-    title = 'Home - Welcome to The best News Website Online'
-    return render_template('index.html', title = title,popular = popular_news)
+    # Getting popular news
+    general_news = get_news('general')
+    sports_news = get_news('sports')
+    entertainment_news = get_news('entertainment')
+    health_news = get_news('health')
+    business_news = get_news('business')
+    tech_news = get_news('technology')
+    science_news = get_news('science')
+    title = 'Welcome to the best News website'
+    return render_template('index.html', title = title, general = general_news, sports = sports_news, entertainment = entertainment_news,   health = health_news, business = business_news, technology = tech_news, science = science_news)
 
+    
