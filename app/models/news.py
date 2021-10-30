31ede1c65bf69fac8c0b168d24816dfd3317b027
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-30 13:34:51 +0300

    define a News class and then we create an __init__ method

---
+++ app/models/news.py
@@ -0,0 +1,12 @@
+class News:
+    '''
+    Movie class to define News Objects
+    '''
+
+    def __init__(self,id,title,overview,poster,vote_average,vote_count):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.poster = 'https://newsapi.org/'+ poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
