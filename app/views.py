5f186b1e41b5e0899ff88cb957900c251a1d7f07
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-30 12:42:48 +0300

    create a variable title and pass it into the templates

--- app/views.py
+++ app/views.py
@@ -2,10 +2,19 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/news/<news_id>')
+@app.route('/news/<int:news_id>')
 def news(news_id):
 
     '''
-    View movie page function that returns the movie details page and its data
+    View news page function that returns the news details page and its data
     '''
-    return render_template('movie.html',id = news_id)
+    return render_template('news.html',id = news_id)
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best News Website Online'
+    return render_template('index.html', title = title)
+
