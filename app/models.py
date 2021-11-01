a62cba5e14c032cbbca3f8c91e04246b47eaa9b6
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-11-01 09:57:02 +0300

    models change

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ class News:
     News class that defines the news objects
     '''
     
-    def h(self, id, name, description, url, country):
+    def __init__(self, id, name, description, url, country):
         self.id = id
         self.name = name
         self.description = description
